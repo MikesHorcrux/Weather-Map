bf88df7c76afda10d0d59584d6f69f9a3bd36eb7
justBearded <vanamburg.mv@gmail.com>
2020-07-15 13:55:07 -0500

    Initial Commit

---
+++ Weather Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather Map
+//
+//  Created by Mike Van Amburg on 7/15/20.
+//  Copyright © 2020 Mike Van Amburg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
